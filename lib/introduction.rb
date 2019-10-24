0ab560c15d91dd9a47a04cd5b4e540c178264d10
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-24 17:30:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,8 +19,6 @@ def introduction_with_language_optional(name, language = "Ruby")
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-introduction_with_language_optional("Anna", "Ruby")
-
 introduction_with_language_optional("Leon")
 
 
